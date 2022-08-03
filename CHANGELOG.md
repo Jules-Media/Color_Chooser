a3278bb08dca1602543f9bdc86c21378a9a145b2
Julian Schumacher <Jules.media@outlook.de>
2022-08-03 19:33:10 +0200

    Version 0.0.3
    Add Exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.0.3
+
+* Add Exports
+
+
 ## 0.0.2
 
 * Add Classes for Screens.
--- lib/color_chooser.dart
+++ lib/color_chooser.dart
@@ -1,1 +1,4 @@
 library color_chooser;
+
+export 'src/mobile/color_chooser_screen_mobile.dart';
+export 'src/desktop/color_chooser_screen_desktop.dart';
